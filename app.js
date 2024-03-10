d9f59de3e063740be135944915c87135ceb5fccb
G-Mustafa1 <157616503+G-Mustafa1@users.noreply.github.com>
2024-03-11 01:41:04 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+//     Chapter 4  :  Exercise 1
+
+var a = "",
+  b = "0",
+  c = "true";
+
+//     Chapter 4  :  Exercise 2
+
+console.log(
+  "5 legal variable names.\n\n 1.camelCaseName.\n 2.$price. \n 3._age. \n 4.variable123. \n 5.[legal_Variable]."
+);
+
+console.log(
+  "\n5 illegal variable names.\n\n 1.123name.\n 2.name-age\n 3.var,const,let \n 4.true-or-false-value.\n 5.@name"
+);
+
+// document.write('<br /><h1> 5 legal variable names.</h1>\n<ol><li>camelCaseName.</li><li>$price.</li><li>_age.</li><li>variable123.</li><li>[legal_Variable].</li></ol>');
+
+// document.write('<br /><h1> 5 illegal variable names.</h1>\n<ol><li>123name.</li><li>name-age.</li><li>var, const, let.</li><li>true-or-false-value.</li><li>@name.</li>')
+
+//     Chapter 4  :  Exercise 3 in HTML
